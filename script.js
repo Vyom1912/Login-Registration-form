2669ebc835d57a79ac0a95dd231e939363429dd1
VYOM PATEL <124497900+Vyom1912@users.noreply.github.com>
2025-01-05 15:46:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-burger = document.querySelector(".burger");
-navbar = document.querySelector(".navbar");
-navList = document.querySelector(".nav-list");
-search = document.querySelector(".search");
+const user_con = document.querySelector(".user-container");
+const regis_btn = document.querySelector(".registration-btn");
+const login_btn = document.querySelector(".login-btn");
 
-burger.addEventListener("click", () => {
-  search.classList.toggle("v-class-resp");
-  navList.classList.toggle("v-class-resp");
-  navbar.classList.toggle("h-nav-resp");
+regis_btn.addEventListener("click", () => {
+  user_con.classList.add("login-section--display");
 });
-
-
-
-// --------
+login_btn.addEventListener("click", () => {
+  user_con.classList.remove("login-section--display");
+});
